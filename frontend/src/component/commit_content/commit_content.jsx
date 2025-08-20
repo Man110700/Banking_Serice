cb9c88630541973bf1cec0c5a53cd9b4f399341a
Man110700 <phamngocman00@gmail.com>
2025-08-20 16:45:01 +0700

    fix responsive

--- frontend/src/component/commit_content/commit_content.jsx
+++ frontend/src/component/commit_content/commit_content.jsx
@@ -6,10 +6,14 @@ import icon_check from "../../assets/commit_content/check.png";
 export default function Commit_Content() {
   return (
     <div className="commit-content">
-      <img src={commit_image} alt="commit_image" />
+      <img src={commit_image} alt="commit_image" className="commit-banner" />
       <div className="commit-infor">
         <div className="commit-header">
-          <img src={commit_header} alt="commit_header" />
+          <img
+            src={commit_header}
+            alt="commit_header"
+            className="header-text"
+          />
           <img src={icon_check} alt="icon-check" className="header-icon" />
         </div>
         <ul>
